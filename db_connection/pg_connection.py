11ec10993d26059b8abba9187fdfa6e77920334b
Oleksii Krasnyanskyi <o.krasnyanskyi@gmail.com>
2022-07-15 18:46:43 +0300

    check variable name

--- db_connection/pg_connection.py
+++ db_connection/pg_connection.py
@@ -2,20 +2,20 @@ import psycopg2
 from config_pg import host, user, password, db_name
 
 try:
-    connection = psycopg2.connect(
+    db_connection = psycopg2.connect(
         host=host,
         user=user,
         password=password,
         database=db_name
     )
-    with connection.cursor() as cursor:
+    with db_connection.cursor() as cursor:
         cursor.execute(
             'SELECT version()'
         )
-        print(f'Server sersion: {cursor.fetchone()[0]}')
+        print(f'Server version: {cursor.fetchone()[0]}')
 except Exception as _ex:
     print(f'[INFO] Error while working with PostgreSQL {_ex}')
 finally:
-    if connection:
-        connection.close()
+    if db_connection:
+        db_connection.close()
         print(f'[INFO] PostgreSQL connection closed')
